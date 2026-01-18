7ecfe2feb56fb8e824dce315e472c84d32824dc8
Rafael Silva <rafaelp_silva01@hotmail.com>
2026-01-18 15:59:57 -0300

    Alteração no EntityModel

--- src/main/java/br/projetopessoal/msemail/models/EmailModel.java
+++ src/main/java/br/projetopessoal/msemail/models/EmailModel.java
@@ -18,12 +18,15 @@ public class EmailModel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID emailId;
+
     private String ownerRef;
     private String emailFrom;
     private String emailTo;
     private String subject;
+
     @Column(columnDefinition = "TEXT")
     private String text;
+
     private LocalDateTime sendDateEmail;
     private StatusEmail statusEmail;
 }
